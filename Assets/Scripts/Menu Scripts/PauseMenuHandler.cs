f9da47812126af458eb60e0b24eb3746349d1b8d
Nicole Tiongson <ntiongso@gmu.edu>
2024-11-10 17:08:28 -0500

    Pause Menu prefab

---
+++ Assets/Scripts/Menu Scripts/PauseMenuHandler.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool isPaused= false;
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            togglePause();
+        }
+    }
+
+    public void togglePause()
+    {
+        isPaused = !isPaused;
+        pauseMenu.SetActive(isPaused);
+
+        if (isPaused)
+        {
+            Time.timeScale = 0.0f;
+            //input.enabled = false;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+           // input.enabled = true;
+        }
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+}
