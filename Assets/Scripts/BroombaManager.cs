1607972fb8da97731c83401933ffe02cfc16e474
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-11-09 21:21:36 -0500

    start of broomba manager

---
+++ Assets/Scripts/BroombaManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BroombaManager : MonoBehaviour
+{
+    void Start()
+    {
+        //
+    }
+
+    public void SetActiveBroomba(string name)
+    {
+        // TODO: Set active Broomba
+    }
+}
